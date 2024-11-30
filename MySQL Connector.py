e770abc56140abade560da3cc270745e78d87900
vorpal <81919558+vorpal-ap@users.noreply.github.com>
2024-11-30 10:56:53 +0530

    Create MySQL Connector.py
    Create a table and modify

---
+++ MySQL Connector.py
@@ -0,0 +1,42 @@
+'''
+Create a table and modify
+'''
+
+
+
+import pymysql as sql
+
+connection = sql.connect(host='localhost', user='root', password='pass')
+cursor = connection.cursor()
+
+cursor.execute("CREATE DATABASE DB;")
+cursor.execute("USE DB;")
+cursor.execute("CREATE TABLE Marks ("
+               "Roll INT PRIMARY KEY."
+               "Name CHAR(10) NOT NULL,"
+               "Marks INT NOT NULL"
+               ");")
+cursor.execute("INSERT INTO Marks (Roll, Name, Marks) VALUES"
+               "(1, 'Anish', 40),"
+               "(2, 'Neel', 60),"
+               "(3, 'Arpita', 80);"
+               )
+cursor.execute("UPDATE Marks"
+               " SET Marks = Marks + 20"
+               " WHERE Name LIKE 'A%';")
+cursor.execute("ALTER TABLE Marks"
+               " RENAME COLUMN Name"
+               " TO Student_Name;")
+cursor.execute("ALTER TABLE Marks"
+               " DROP PRIMARY KEY;")
+cursor.execute("INSERT INTO Marks (Roll, Student_Name, Marks) VALUES"
+               "(4, 'Abhay', 20);")
+cursor.execute("DELETE FROM Marks"
+               " WHERE Marks < 50;")
+
+print("OUTPUT ----------------")
+data = cursor.fetchall()
+for line in data:
+    print(line)
+cursor.close()
+connection.close()
