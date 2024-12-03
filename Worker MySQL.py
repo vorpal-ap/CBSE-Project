0a453340e1347ca4fdde33e38c9f9ea29f0cc107
vorpal <81919558+vorpal-ap@users.noreply.github.com>
2024-12-03 22:45:44 +0530

    Update Worker MySQL.py
    Print SQL output fixed

--- Worker MySQL.py
+++ Worker MySQL.py
@@ -8,17 +8,24 @@ import pymysql as sql
 connection = sql.connect(host='localhost', user='root', password='pass')
 cursor = connection.cursor()
 
+def printSQL():
+    for line in cursor.fetchall():
+        print(line)
+    print()
+
+
+cursor.execute("DROP DATABASE DB;")
 cursor.execute("CREATE DATABASE DB;")
 cursor.execute("USE DB;")
 cursor.execute("CREATE TABLE WORKER ("
-               "ECODE INT PRIMARY KEY."
+               "ECODE INT PRIMARY KEY,"
                "NAME CHAR(50),"
                "DESIG CHAR(50),"
                "PLEVEL CHAR(4),"
                "DOJ DATE,"
                "DOB DATE"
                ");")
-cursor.execute("INSERT INTO WORKER (ECODE, NAME, DESIG, PLEVEL, DOJ, DOB) VALUES"
+cursor.execute("INSERT INTO WORKER VALUES"
                "(11, 'Radhe Shyam', 'Supervisor', 'P001', '2004-09-13', '1981-08-23'),"
                "(12, 'Chander Nath', 'Operator', 'P003', '2010-02-22', '1987-07-12'),"
                "(13, 'Fizza', 'Operator', 'P003', '2009-06-14', '1983-10-14'),"
@@ -26,13 +33,14 @@ cursor.execute("INSERT INTO WORKER (ECODE, NAME, DESIG, PLEVEL, DOJ, DOB) VALUES
                "(18, 'Sanya', 'Clerk', 'P002', '2005-12-19', '1983-06-09');")
 
 cursor.execute("SELECT * FROM WORKER ORDER BY DOB DESC;")
-cursor.execute("SELECT NAME, DESIG FROM WORKER WHERE PLEVEL is 'P001' or PLEVEL is 'P002';")
+printSQL()
+cursor.execute("SELECT NAME, DESIG FROM WORKER WHERE PLEVEL = 'P001' or PLEVEL = 'P002';")
+printSQL()
 cursor.execute("SELECT * FROM WORKER WHERE DOB BETWEEN '1984-01-19' AND '1987-01-18';")
+printSQL()
 cursor.execute("INSERT INTO WORKER VALUES (19, 'Daya Kishor', 'Operator', 'P003', '2008-06-18', '1984-07-11');")
+cursor.execute("SELECT * FROM WORKER")
+printSQL()
 
-print("OUTPUT ----------------")
-data = cursor.fetchall()
-for line in data:
-    print(line)
 cursor.close()
 connection.close()
